4bdf2f77b23241fa3cbf4db7d81e89b6f68e7091
Olerato. <olerato.mosikidi@gmail.com>
2025-06-17 16:29:59 +0200

    refactor: add semi-colons. feat: declare new function that fills in data from getData into the html spans. feat: add a key for windspeed in currentForecast.

--- script.js
+++ script.js
@@ -3,38 +3,39 @@ const ficksburg = JSON.parse(localStorage.getItem('ficksburg'));
 
 async function getData(city){
   
-  operateLoadingScreen(true)
+  operateLoadingScreen(true);
   
   try {
   
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=YA3JYGZUNLJTQX73DKKAMD87Q&unitGroup=uk`);
     const data = await response.json();
     const forecast = await currentForecast(data);
-    return forecast;
+    fillDetails(forecast);
  
  
   } catch(error){
     console.error('There was a problem.');
   } finally {
-    operateLoadingScreen(false)
+    operateLoadingScreen(false);
   }
 }
 
 function getPostalAddress(data){
   return data.resolvedAddress
           .split((data.address + ","))
-            .join('').trim()
+            .join('').trim();
 }
 
 function currentForecast(data){
   return {
     city: data.address,
     postalAddress: getPostalAddress(data),
-    descrip: data.description,
+    description: data.description,
     currentTemp: data.currentConditions.temp,
     maxTemp: data.days[0].tempmax,
     minTemp: data.days[0].tempmin,
-    humidity: Math.floor(data.currentConditions.humidity),
+    humidity: Math.floor(data.currentConditions.humidity) + '%',
+    wind: Math.floor(data.days[0].windspeed) + 'km/h'
   };
 }
 
@@ -43,19 +44,19 @@ function currentForecast(data){
 async function ficks() {
   
   try{
-  operateLoadingScreen(true)
+  operateLoadingScreen(true);
    const h = await new Promise((resolve, reject) => {
     setTimeout(() => { return resolve( JSON.parse(localStorage.getItem('ficks')))}
       , 2000);
-   })
+   });
    
-   console.log(currentForecast(h))
+   console.log(currentForecast(h));
 
   
 }catch (e){
-  console.error('Boom')
+  console.error('Boom');
 } finally {
-  operateLoadingScreen(false)
+  operateLoadingScreen(false);
 }
 }
 
@@ -63,12 +64,30 @@ const loadingScreen = document.getElementById('loading-screen');
 
 function operateLoadingScreen(status){
   if(!status){
-    loadingScreen.innerHTML = 'Done!'
-    loadingScreen.style.display = 'none'
+    loadingScreen.innerHTML = 'Done!';
+    loadingScreen.style.display = 'none';
   } else {
     loadingScreen.style.display = "block";
-    loadingScreen.innerHTML += "."
+    loadingScreen.innerHTML += ".";
   }
 }
 
-getData('Barbados')
+getData('Barbados');
+
+const details = document.getElementById('details');
+
+const detailFields = details.querySelectorAll('span');
+
+function fillDetails(data){
+  detailFields.forEach((field) => {
+    
+    let fieldClass = field.classList[0];
+    
+    if(Object.keys(data).includes(fieldClass)){
+      field.innerHTML = data[fieldClass];
+    } else if (Object.keys(data).includes(`${fieldClass}Temp`)){
+      field.innerHTML = data[`${fieldClass}Temp`] + '°C';
+    }
+  })
+}
+
