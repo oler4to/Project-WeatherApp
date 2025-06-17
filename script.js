d9d57e864682a0b25c7d0985d996a63e91c4b524
Olerato. <olerato.mosikidi@gmail.com>
2025-06-17 15:35:29 +0200

    feat: add finally statement to getData. feat: add a function that starts a loading screen. ignore: ficks is a test function. refactor: get rid of convertToCelcius refactor: change getResolvedAdd to getPostalAddress that get the postal address.

--- script.js
+++ script.js
@@ -2,33 +2,73 @@
 const ficksburg = JSON.parse(localStorage.getItem('ficksburg'));
 
 async function getData(city){
+  
+  operateLoadingScreen(true)
+  
   try {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=YA3JYGZUNLJTQX73DKKAMD87Q`);
+  
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=YA3JYGZUNLJTQX73DKKAMD87Q&unitGroup=uk`);
     const data = await response.json();
-    console.log(data);
+    const forecast = await currentForecast(data);
+    return forecast;
+ 
+ 
   } catch(error){
     console.error('There was a problem.');
+  } finally {
+    operateLoadingScreen(false)
   }
 }
 
-function getResolvedAdd(address){
-  return address.slice(address.indexOf(' ')+1);
-}
-
-function convertToCelcius(temp){
-  return Math.round((temp-32)*(5/9));
+function getPostalAddress(data){
+  return data.resolvedAddress
+          .split((data.address + ","))
+            .join('').trim()
 }
 
 function currentForecast(data){
   return {
     city: data.address,
-    resolvedAddress: getResolvedAdd(data.resolvedAddress),
+    postalAddress: getPostalAddress(data),
     descrip: data.description,
-    currentTemp: convertToCelcius(data.currentConditions.temp),
-    maxTemp: convertToCelcius(data.days[0].tempmax),
-    minTemp: convertToCelcius(data.days[0].tempmin),
-    humidity: convertToCelcius(data.currentConditions.humidity),
+    currentTemp: data.currentConditions.temp,
+    maxTemp: data.days[0].tempmax,
+    minTemp: data.days[0].tempmin,
+    humidity: Math.floor(data.currentConditions.humidity),
   };
 }
 
 
+
+async function ficks() {
+  
+  try{
+  operateLoadingScreen(true)
+   const h = await new Promise((resolve, reject) => {
+    setTimeout(() => { return resolve( JSON.parse(localStorage.getItem('ficks')))}
+      , 2000);
+   })
+   
+   console.log(currentForecast(h))
+
+  
+}catch (e){
+  console.error('Boom')
+} finally {
+  operateLoadingScreen(false)
+}
+}
+
+const loadingScreen = document.getElementById('loading-screen');
+
+function operateLoadingScreen(status){
+  if(!status){
+    loadingScreen.innerHTML = 'Done!'
+    loadingScreen.style.display = 'none'
+  } else {
+    loadingScreen.style.display = "block";
+    loadingScreen.innerHTML += "."
+  }
+}
+
+getData('Barbados')
