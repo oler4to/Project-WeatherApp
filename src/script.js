86faf24c0c3451e8ec58cb1ba38013026cc471c1
Olerato. <olerato.mosikidi@gmail.com>
2025-06-26 15:30:09 +0200

    feat: import search icon. feat: target searchButton that will run getData with searchInput (targeted input from search bar) as a parameter onclick. feat: declare method hour in forcast, that will return a specific hour based on the time. feat: declare changeBackground function, that will change the webpages background based on a specific hour. refactor: delete ficks().

--- src/script.js
+++ src/script.js
@@ -1,7 +1,15 @@
 import './styles.css';
+import searchIcon from './assets/search-icon-white.png';
 
+const searchInput = document.querySelector('header #search-bar .search-input');
+const searchButton = document.querySelector('header #search-bar button');
 
-const ficksburg = JSON.parse(localStorage.getItem('ficksburg'));
+    searchButton.firstElementChild.src = searchIcon;
+    
+    searchButton.onclick = () => {
+      getData(searchInput.value);
+      searchInput.value = '';
+    };
 
 async function getData(city){
   
@@ -12,8 +20,9 @@ async function getData(city){
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=YA3JYGZUNLJTQX73DKKAMD87Q&unitGroup=uk`);
     const data = await response.json();
     const forecast = await currentForecast(data);
-    console.log(forecast);
+    
     fillDetails(forecast);
+    changeBackground(forecast);
  
  
   } catch(error){
@@ -44,6 +53,7 @@ function currentForecast(data){
       postalAddress: getPostalAddress(data),
     },
     description: data.description,
+    condition: data.currentConditions.conditions,
     temps: {
       current: data.currentConditions.temp,
       min: data.days[0].tempmin,
@@ -66,61 +76,95 @@ function currentForecast(data){
         forecast.times[time] = forecast.times[time].slice(0, -3);
       }
     })();
+    
+    function hour(){
+      let currentTime = forecast.times.current;
+      let currentHour = Number(currentTime.slice(0, currentTime.search(':')));
+      
+      let hours = [6, 12, 17, 20];
+      
+      hours.every((hour) => {
+        if(currentHour < 12){
+          forecast.hour = 6;
+        } else if(currentHour < 17){
+          forecast.hour = 12;
+        } else if(currentHour < 20){
+          forecast.hour = 17;
+        } else if(currentHour < 6 || currentHour >= 20) {
+          forecast.hour = 20;
+        }
+      });
+    }
+    
+    hour();
   
   return forecast;
 }
 
-
-
-async function ficks() {
-  
-  try{
-  operateLoadingScreen(true);
-   const h = await new Promise((resolve, reject) => {
-    setTimeout(() => { return resolve( JSON.parse(localStorage.getItem('ficks')))}
-      , 2000);
-   });
-   
-   console.log(currentForecast(h));
-
-  
-}catch (e){
-  console.error('Boom');
-} finally {
-  operateLoadingScreen(false);
-}
-}
-
 const loadingScreen = document.getElementById('loading-screen');
+
+const welcome = document.getElementById('welcome');
 
 function operateLoadingScreen(status){
   if(!status){
     loadingScreen.innerHTML = 'Getting Data <br/>';
     loadingScreen.style.display = 'none';
   } else {
+    forecast.style.display = 'none';
+    welcome.style.display = 'none';
     loadingScreen.style.display = "block";
+    
     setInterval(function(){
-      
     if(!loadingScreen.innerHTML.includes('...')){
         loadingScreen.innerHTML += '.';
-    } else loadingScreen.innerHTML = 'Getting Data <br/>';
-  } , 400);
+    } else loadingScreen.innerHTML = '<span>Getting Data</span> <br/>';
+      
+    } , 600);
     
+  }}
+
+function changeBackground(data){
+  const background = document.querySelector('body');
+  
+  let colours = {
+    6: ['#255389', '#002d61', '#7d97c0'],
+    12: ['skyblue', '#d7d682', '#c8bb32'],
+    17: ['#6f3800', '#0a0a1a'],
+    20: ['#000526', '#090d20', '#000e14']
+  };
+  
+  if(Object.keys(colours).includes(data.hour.toString())){
+    let colourIndex = Number(data.hour);
+    
+    if(colours[data.hour].length < 3){
+      background.style.background = `
+      linear-gradient(
+        ${colours[colourIndex][0]},
+        ${colours[colourIndex][1]}
+      )`;
+    } else {
+      background.style.background = `
+        linear-gradient(
+          ${colours[colourIndex][0]},
+          ${colours[colourIndex][1]},
+          ${colours[colourIndex][2]}
+        )`;
+    }
   }
 }
 
-getData('Ficksburg');
-
-const detailFields = document.querySelectorAll('#details > span');
+const forecast = document.getElementById('forecast');
 
 function fillDetails(data){
+  forecast.style.display = 'flex';
+  
   detailFields.forEach((field) => {
     
     let fieldClass = field.className;
     
     if(Object.keys(data).includes(fieldClass) && fieldClass !== 'description'){
         
-        import (`./icons/${data.conditions.icon}.svg`)
+        import (`./assets/${data.conditions.icon}.svg`)
           .then((icon) => {
             field
               .firstElementChild
@@ -132,11 +176,22 @@ function fillDetails(data){
             let childClass = child.className;
             
             if(Object.keys(data[fieldClass]).includes(childClass)){
-              child.innerHTML = data[fieldClass][childClass];
+              child.lastElementChild.innerHTML = data[fieldClass][childClass];
             }
           
         });
           
-      } else field.innerHTML = data[fieldClass];
+      } else field.innerHTML = `
+      <p>
+        <strong>Conditions:</strong>
+              ${data.condition}
+      </p>
+      <br/>
+      <br/>
+       <p>
+        <strong>Description:</strong>
+              ${data.description}
+      </p>
+      `;
   });
 }
